1851803e88f8617d6e84d91d333d33afa05384b8
Administrator <Administrator@DESKTOP-6IC4J3S>
2020-10-01 13:24:07 +0330

    Add project files.

---
+++ ConnectServerWithSignalR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConnectServerWithSignalR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
